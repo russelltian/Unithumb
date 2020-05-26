a33ba982271cf34db1435941447e50ab81c865f8
russell tian <russell.tian1996@gmail.com>
2020-05-25 21:15:09 -0400

    start android version

--- Podfile
+++
@@ -1,5 +0,0 @@
-source 'https://github.com/CocoaPods/Specs.git'
-target 'Hackthe_sleep' do
-pod 'GoogleMaps'
-pod 'GooglePlaces'
-end
--- Pods/Target Support Files/Pods-Hackthe_sleep/Pods-Hackthe_sleep-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Hackthe_sleep : NSObject
-@end
-@implementation PodsDummy_Pods_Hackthe_sleep
-@end
