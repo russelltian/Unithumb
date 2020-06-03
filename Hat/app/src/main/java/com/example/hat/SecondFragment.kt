ef506f9f00c064c4d5022d166e1558c0641e1f44
Hongyi Tian <russell.tian1996@gmail.com>
2020-06-03 00:22:25 -0400

    - Add socket object so only one in the program - remove disconnect to test avaliablity of the socket

--- Hat/app/src/main/java/com/example/hat/SecondFragment.kt
+++ Hat/app/src/main/java/com/example/hat/SecondFragment.kt
@@ -12,14 +12,13 @@ import androidx.navigation.fragment.findNavController
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class SecondFragment : Fragment() {
-    var socket_obj: SocketClient? = null
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        socket_obj = SocketClient(address = "10.0.2.2", port = 8000)
+        //socket_obj = SocketClient(address = "10.0.2.2", port = 8000)
         return inflater.inflate(R.layout.fragment_second, container, false)
     }
 
@@ -27,13 +26,13 @@ class SecondFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         view.findViewById<Button>(R.id.button_left_signal).setOnClickListener {
-            socket_obj?.sendMessage("left")
+            SocketInstance.sendMessage("left")
         }
         view.findViewById<Button>(R.id.button_right_signal).setOnClickListener {
-            socket_obj?.sendMessage("right")
+            SocketInstance.sendMessage("right")
         }
         view.findViewById<Button>(R.id.button_return_main).setOnClickListener {
-            socket_obj?.close()
+            SocketInstance.close()
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
     }
--- Hat/app/src/main/java/com/example/hat/SocketClient.kt
+++ Hat/app/src/main/java/com/example/hat/SocketClient.kt
@@ -1,8 +1,8 @@
 package com.example.hat
-import java.net.InetSocketAddress
 import java.nio.ByteBuffer
 import java.nio.channels.AsynchronousSocketChannel
 import kotlin.concurrent.thread
+import java.net.InetSocketAddress as InetSocketAddress
 
 /*
 There should be only one socket object that connects with the hat
@@ -55,5 +55,41 @@ class SocketClient(address: String, port: Int){
     fun close(){
         client.close()
         connected = false
+    }
+}
+/*
+Socket instance that only has one copy in the program
+ */
+object SocketInstance {
+    var client: AsynchronousSocketChannel = AsynchronousSocketChannel.open()
+    var connected:Boolean = false
+    var hostAddress: InetSocketAddress? = null
+
+    fun setup(address: String, port: Int){
+        hostAddress = InetSocketAddress(address, port)
+        println("Connecting to server at $address on port $port")
+    }
+
+    fun sendMessage(message: String){
+        thread{
+            try {
+                if (!connected) {
+                    val future = client.connect(hostAddress)
+                    future.get() // should return NULL
+                    connected = true
+                }
+                if (BuildConfig.DEBUG && !client.isOpen) {
+                    error("Assertion failed, the connection socket is not opened")
+                }
+                client.write(ByteBuffer.wrap(message.toByteArray()))
+            } catch (e:Exception){
+                println("Socket failed to connect : $e")
+            }
+        }
+    }
+    fun close(){
+        //client.close()
+        println("Close connection")
+        //connected = false
     }
 }
