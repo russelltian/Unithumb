a2b42cd93724a380d724bb20c30c5414cd4cbb1f
Hongyi Tian <russell.tian1996@gmail.com>
2020-06-04 20:02:40 -0400

    1. Add permission to location 2.add map box package 3. add a button does not have function yet 4. add a seperate view for the map 5. add mapbox view in the seperate view 6. add function that call the seperate view for the map

--- Hat/app/src/main/java/com/example/hat/MainActivity.kt
+++ Hat/app/src/main/java/com/example/hat/MainActivity.kt
@@ -3,20 +3,37 @@ package com.example.hat
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.Style
 
 class MainActivity : AppCompatActivity() {
+    private var mapView: MapView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Mapbox access token is configured here. This needs to be called either in your application
+        // object or in the same activity which contains the mapview.
+        Mapbox.getInstance(this, getString(R.string.mapbox_access_token))
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
+
+
 //        SocketInstance.setup(address = "10.0.2.2", port = 8001)
         SocketInstance.setup(address = "192.168.0.29", port = 8000)
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener {
 //                view ->Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                    .setAction("Connect", createSocketClient()).show()
-//            createSocketClient()
+            setContentView(R.layout.map_navigation)
+            mapView = findViewById(R.id.mapView)
+            mapView?.onCreate(savedInstanceState)
+            mapView?.getMapAsync { mapboxMap ->
+                mapboxMap.setStyle(Style.MAPBOX_STREETS) {
+                    // Map is set up and the style has loaded. Now you can add data or make other map adjustments
+                }
+            }
         }
     }
 
