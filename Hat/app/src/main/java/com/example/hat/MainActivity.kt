60f57815888e480570407a5eac1549617ea755d1
Hongyi Tian <russell.tian1996@gmail.com>
2020-06-04 21:05:26 -0400

    commit to correct branch

--- Hat/app/src/main/java/com/example/hat/MainActivity.kt
+++ Hat/app/src/main/java/com/example/hat/MainActivity.kt
@@ -5,18 +5,33 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.Style
 
 class MainActivity : AppCompatActivity() {
+    private var mapView: MapView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
+
+        /*
+        Mapbox access token is configured here. This needs to be called either in your application
+        object or in the same activity which contains the mapview.
+        */
+        Mapbox.getInstance(this, getString(R.string.mapbox_access_token))
 //        SocketInstance.setup(address = "10.0.2.2", port = 8001)
         SocketInstance.setup(address = "192.168.0.29", port = 8000)
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener {
-//                view ->Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                    .setAction("Connect", createSocketClient()).show()
-//            createSocketClient()
+            setContentView(R.layout.map_navigation)
+            mapView = findViewById(R.id.mapView)
+            mapView?.onCreate(savedInstanceState)
+            mapView?.getMapAsync { mapboxMap ->
+                mapboxMap.setStyle(Style.MAPBOX_STREETS) {
+                    // Map is set up and the style has loaded. Now you can add data or make other map adjustments
+                }
+            }
         }
     }
 
