f086c5b409bdda49570ba118b4f067922e43a890
Hongyi Tian <russell.tian1996@gmail.com>
2020-06-07 01:59:59 -0400

    1. Add a new seperate activity for map navigation page 2. add layout file 3. add dependency as the child of main activity in the manifest.xml 4. add onclick fucntion for the left top corner to switch to the map

---
+++ Hat/app/src/main/java/com/example/hat/MapNavigationActivity.kt
@@ -0,0 +1,26 @@
+package com.example.hat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.Style
+
+class MapNavigationActivity : AppCompatActivity() {
+    private var mapView: MapView? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Mapbox.getInstance(this, getString(R.string.mapbox_access_token))
+
+        setContentView(R.layout.activity_map_navigation)
+
+        mapView = findViewById(R.id.mapView)
+        mapView?.onCreate(savedInstanceState)
+        mapView?.getMapAsync { mapboxMap ->
+            mapboxMap.setStyle(Style.MAPBOX_STREETS) {
+                // Map is set up and the style has loaded. Now you can add data or make other map adjustments
+            }
+        }
+    }
+}
