3b50a79c5780d4fb00f89e55035316837decde11
Hongyi Tian <russell.tian1996@gmail.com>
2020-06-09 17:24:42 -0400

    1.Add Symbol Manger to the app, now the map will show a marker on the place you click on 2. documentation is poor so all figure out by myself

--- Hat/app/src/main/java/com/example/hat/MapNavigationActivity.kt
+++ Hat/app/src/main/java/com/example/hat/MapNavigationActivity.kt
@@ -3,11 +3,17 @@ package com.example.hat
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.mapbox.android.core.permissions.PermissionsListener
 import com.mapbox.android.core.permissions.PermissionsManager
+import com.mapbox.api.directions.v5.models.DirectionsResponse
+import com.mapbox.geojson.Point
 import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.annotations.IconFactory
+import com.mapbox.mapboxsdk.geometry.LatLng
 import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions
 import com.mapbox.mapboxsdk.location.LocationComponentOptions
 import com.mapbox.mapboxsdk.location.modes.CameraMode
@@ -16,11 +22,27 @@ import com.mapbox.mapboxsdk.maps.MapView
 import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
 import com.mapbox.mapboxsdk.maps.Style
+import com.mapbox.mapboxsdk.plugins.annotation.Symbol
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolManager
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolOptions
+import com.mapbox.mapboxsdk.style.layers.Property.ICON_ROTATION_ALIGNMENT_VIEWPORT
+import com.mapbox.mapboxsdk.style.layers.PropertyFactory
+import com.mapbox.services.android.navigation.v5.navigation.NavigationRoute
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class MapNavigationActivity: AppCompatActivity(),OnMapReadyCallback,PermissionsListener{
+class MapNavigationActivity: AppCompatActivity(),OnMapReadyCallback,PermissionsListener,MapboxMap.OnMapClickListener
+{
     private var mapView: MapView? = null
     private var map: MapboxMap?= null
     private var permissionsManager: PermissionsManager = PermissionsManager(this)
+
+    private var startButton: Button?=null
+    private var navigationMapRoute: NavigationRoute? = null
+
+    //annotation
+    private var symbolManager:SymbolManager?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +55,10 @@ class MapNavigationActivity: AppCompatActivity(),OnMapReadyCallback,PermissionsL
         mapView = findViewById(R.id.mapView)
         mapView?.onCreate(savedInstanceState)
         mapView?.getMapAsync(this)
+
+        startButton?.setOnClickListener{
+
+        }
     }
 
     @SuppressLint("MissingPermission")
@@ -71,9 +97,64 @@ class MapNavigationActivity: AppCompatActivity(),OnMapReadyCallback,PermissionsL
     }
     override fun onMapReady(mapboxMap: MapboxMap) {
         this.map = mapboxMap
-        mapboxMap.setStyle(Style.MAPBOX_STREETS) {
+        mapboxMap.addOnMapClickListener{click->
+            symbolManager?.deleteAll()
+            symbolManager?.create(
+                SymbolOptions()
+                    .withLatLng(LatLng(click.latitude,click.longitude))
+                    .withIconImage("666")
+                    .withIconSize(2.0f)
+                // .withGeometry(Point.fromLngLat(click.longitude,click.latitude))
+
+            )
+            false
+        }
+        mapboxMap.setStyle(Style.MAPBOX_STREETS) {style->
             // Madp is set up and the style has loaded. Now you can add data or make other map adjustments
-            enableLocationComponent(it)
+            enableLocationComponent(style)
+            //symbolManager = mapView?.let { SymbolManager(it,mapboxMap,style) }
+            symbolManager = mapView?.let { SymbolManager(it,mapboxMap, style) }
+            // set non-data-driven properties, such as:
+            symbolManager?.iconAllowOverlap = true
+            symbolManager?.iconIgnorePlacement = true
+            symbolManager?.iconTranslate = arrayOf(-4f, 5f)
+            symbolManager?.iconRotationAlignment = ICON_ROTATION_ALIGNMENT_VIEWPORT
+            style.addImage("666", IconFactory.getInstance(this).defaultMarker().bitmap)
+        }
+    }
+
+    override fun onMapClick(point: LatLng): Boolean {
+        //destinationMarker = map?.addMarker(MarkerOptions())
+         //   .position(LatLng(37.3861,122.0839))
+          //  .title("Google")
+        return true
+    }
+    private fun getRoute(origin: Point, destination: Point){
+        Mapbox.getAccessToken()?.let {
+            NavigationRoute.builder(this)
+                .accessToken(it)
+                .origin(origin)
+                .destination(destination)
+                .build()
+                .getRoute(object:Callback<DirectionsResponse>{
+                    @SuppressLint("LogNotTimber")
+                    override fun onResponse(
+                        call: Call<DirectionsResponse>,
+                        response: Response<DirectionsResponse>
+                    ) {
+                        val routeResponse = response ?: return
+                        val body = routeResponse.body() ?:return
+                        if (body.routes().count() == 0){
+                            Log.e("MapNavigationActivity","No route found.")
+                            return
+                        }
+                      //   navigationMapRoute = NavigationRoute(body.routes())
+                    }
+
+                    override fun onFailure(call: Call<DirectionsResponse>, t: Throwable) {
+                        TODO("Not yet implemented")
+                    }
+                })
         }
     }
     override fun onRequestPermissionsResult(
