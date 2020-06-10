1369f41c71090a47afe20609bfd0335859cc9aa1
daweishi <sdw.1997.sdw.1997@gmail.com>
2020-06-10 14:41:13 -0400

    Cleanning up

--- Hat/app/src/main/java/com/example/hat/MainActivity.kt
+++ Hat/app/src/main/java/com/example/hat/MainActivity.kt
@@ -13,11 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
-//        SocketInstance.setup(address = "10.0.2.2", port = 8001)
         SocketInstance.setup(address = "192.168.0.29", port = 8000)
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener {
-        }
     }
 
     fun switchToNavigationPage(view: View) {
@@ -28,19 +24,4 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
 }
