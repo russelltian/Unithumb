6177d1513a4ed84b33ac1bc338326a1e01a149b5
russell tian <russell.tian1996@gmail.com>
2020-06-29 18:23:09 -0400

    change socketclient from run time channel to one single channel

--- Hat/app/src/main/java/com/example/hat/SocketClient.kt
+++ Hat/app/src/main/java/com/example/hat/SocketClient.kt
@@ -1,9 +1,76 @@
 package com.example.hat
 import java.nio.ByteBuffer
 import java.nio.channels.AsynchronousSocketChannel
+import java.util.concurrent.Future
+import java.util.concurrent.TimeUnit
 import kotlin.concurrent.thread
 import java.net.InetSocketAddress as InetSocketAddress
 
+/*
+Socket instance that only has one copy in the program
+ */
+object SocketInstance {
+    var client: AsynchronousSocketChannel = AsynchronousSocketChannel.open()
+    var hostAddress: InetSocketAddress? = null
+    var future: Future<Void>? = null
+    //called when starting the app
+    fun setup(address: String, port: Int){
+        hostAddress = InetSocketAddress(address, port)
+        println("Connecting to server at $address on port $port")
+    }
+    // check the channel is connected
+    fun connected(): Boolean {
+        if (future == null) return false
+        return (future!!.get(100, TimeUnit.MILLISECONDS) == null)
+    }
+
+    fun connect(){
+        try {
+            println("Socket connecting ......")
+            if (future == null){
+                future = client.connect(hostAddress)
+            }
+            var counter = 10
+            while(!connected()&& counter > 0){
+                future = client.connect(hostAddress)
+                counter -= 1
+            }
+            if (!connected()){
+                error("Assertion failed, the socket failed to connect")
+            }
+                val future = client.connect(hostAddress)
+                future.get() // should return NULL
+
+            if (!client.isOpen) {
+                error("Assertion failed, the connection socket is not opened")
+            }
+            println("Socket is connected")
+        } catch (e:Exception){
+            println("Socket failed to connect : $e")
+        }
+    }
+    fun sendMessage(message: String){
+       thread{
+            try {
+                if (!connected()) {
+                    println("Socket not connected, trying connect")
+                    connect()
+                }
+                client.write(ByteBuffer.wrap(message.toByteArray()))
+            } catch (e:Exception){
+                println("Socket failed to connect : $e")
+            }
+       }
+    }
+    fun close(){
+        client.close()
+        println("Close connection")
+    }
+}
+
+
+
+// we don't use this one by far
 /*
 There should be only one socket object that connects with the hat
 
@@ -57,42 +124,5 @@ object SocketClient{
     fun close(){
         client.close()
         connected = false
-    }
-}
-/*
-Socket instance that only has one copy in the program
- */
-object SocketInstance {
-    var client: AsynchronousSocketChannel = AsynchronousSocketChannel.open()
-    var connected:Boolean = false
-    var hostAddress: InetSocketAddress? = null
-
-    fun setup(address: String, port: Int){
-        hostAddress = InetSocketAddress(address, port)
-        println("Connecting to server at $address on port $port")
-    }
-
-    fun sendMessage(message: String){
-        thread{
-            try {
-                if (!connected) {
-                    print("Socket not connected yet")
-                    val future = client.connect(hostAddress)
-                    future.get() // should return NULL
-                    connected = true
-                }
-                if (BuildConfig.DEBUG && !client.isOpen) {
-                    error("Assertion failed, the connection socket is not opened")
-                }
-                client.write(ByteBuffer.wrap(message.toByteArray()))
-            } catch (e:Exception){
-                println("Socket failed to connect : $e")
-            }
-        }
-    }
-    fun close(){
-        //client.close()
-        println("Close connection")
-        //connected = false
     }
 }
